bae32768ec843f04b65df16f22fdc39092535a98
Jeremy Meyer <jjman505@gmail.com>
2015-12-02 15:09:33 +0900

    commit

--- game.js
+++ game.js
@@ -1,6 +1,55 @@
+
+/*************************************
+* EXPRESS SERVER CONFIG              *
+*************************************/
+
+// Dependencies
+var express = require('express.io'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
+
+// Express Configuration
+var app = express();
+app.http().io()
+
+app.listen(7000);
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', routes.index);
+
+// Start server
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*************************************
+* SOCKET.IO GAME HANDLERS            *
+*************************************/
+// Dependencies
 var util = require("util"),
-    io = require("socket.io"),
-    Player = require("./Player").Player,
+    io = require("socket.io");
+
+// Server Objects
+var Player = require("./Player").Player,
     House = require("./House").House;
 
 var socket,
